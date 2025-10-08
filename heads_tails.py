e00c04b9fc25094b8c23a4ebe16cd67b3918110c
IzaiasZIPL <Izaiasjrterto@gmail.com>
2025-10-07 23:57:03 -0300

    ✨feat: adiciona a opção do melhor de três

--- heads_tails.py
+++ heads_tails.py
@@ -1,9 +1,14 @@
 import random 
 from time import sleep
+import sys
 
 options = ["Cara", "Coroa"]
 
 print('Bem vindo a divinação com moedas!\n\nFaça a sua pergunta, tente ser o máximo específico para que a moeda entenda sua pergunta e o ajude!\nNão abuse muito, tente um melhor de três para cada pergunta.\n')
+
+question = input("Digite sua pergunta, para que a moeda o responda! ")
+
+CoinSide = input("Qual lado da moeda equivale a sim? Digite Cara ou Coroa: ")
 
 def best_of_three(): 
     while(True):
@@ -20,14 +25,48 @@ def best_of_three():
             if option == "sim":
                 continue
             elif option == "não":
-                break
-            
+                sys.exit()         
         else:
             print("Coroa é \"sim\", cara é \"não\".")
             print(question)
             sleep(3)
             print(random.choice(options))
             print(random.choice(options))
+            print(random.choice(options))    
+            
+            
+            option = input("Deseja continuar? sim/não: ")
+
+            if option == "sim":
+                continue
+            elif option == "não":
+                sys.exit()    
+    
+    
+            
+while True:
+    option_bestof3 = input("Deseja ir o modo normal ou um melhor de três(Você receberá o resultado de três moedas e a que se repetir mais de uma vez é a resposta correta)?\nDigite \"modo normal\" ou \"melhor de tres\".\nSe deseja parar a divinação digite \"não\". ")
+    
+    if option_bestof3 == "modo normal":
+        print(question)
+        sleep(1)
+        if CoinSide == "cara":
+            print("Cara é \"sim\", coroa é \"não\".")
+            print(question)
+            sleep(3)
+            print(random.choice(options))
+            
+            option = input("Deseja continuar? sim/não: ")
+
+            if option == "sim":
+                continue
+            elif option == "não":
+                break
+
+        else:
+            print("Coroa é \"sim\", cara é \"não\".")
+            print(question) 
+            sleep(3)
             print(random.choice(options))
 
             option = input("Deseja continuar? sim/não: ")
@@ -36,36 +75,12 @@ def best_of_three():
                 continue
             elif option == "não":
                 break
+    elif option_bestof3 == 'melhor de três' or 'melhor de tres':
+        best_of_three()
         
-    pass
-
-while True:
-    question = input("Digite sua pergunta, para que a moeda o responda! ")
+    elif option_bestof3 == 'não' or 'nao':
+        sys.exit()
     
-    CoinSide = input("Qual lado da moeda equivale a sim? Digite Cara ou Coroa: ")
-    sleep(1)
-    if CoinSide == "cara":
-        print("Cara é \"sim\", coroa é \"não\".")
-        print(question)
-        sleep(3)
-        print(random.choice(options))
-        
-        option = input("Deseja continuar? sim/não: ")
-
-        if option == "sim":
-            continue
-        elif option == "não":
-            break
-
-    else:
-        print("Coroa é \"sim\", cara é \"não\".")
-        print(question) 
-        sleep(3)
-        print(random.choice(options))
-
-        option = input("Deseja continuar? sim/não: ")
-
-        if option == "sim":
-            continue
-        elif option == "não":
-            break
+    else: 
+        print('digite uma resposta válida')
+        continue
